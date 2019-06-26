04bb447b29d3b5f1b66f5f7e60572b1ec19a0efb
Luis <luis@gsoft.es>
2019-06-26 15:13:35 +0200

    fixe

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'gsoft_es_es',
+    'gsoftes_es_es',
     __DIR__
 );
